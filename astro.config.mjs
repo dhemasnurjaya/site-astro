5fad766ae30ffd55c7e71ad6eb745415f2e5833c
Dhemas Nurjaya <dhemasnurjaya@gmail.com>
2026-01-24 12:56:29 +0700

    feat: integrate astro-pagefind for enhanced search functionality and update related components

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [pagefind()],
   vite: {
     plugins: [tailwindcss()],
   },
