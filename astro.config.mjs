c9ae136996467446568d3cf7d50de2cd463dbc12
Dhemas Nurjaya <dhemasnurjaya@gmail.com>
2026-02-17 09:49:08 +0700

    use expressive code for code styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  markdown: {
-    shikiConfig: {
-      theme: 'dark-plus',
-    }
-  }
+  integrations: [expressiveCode()]
 });
